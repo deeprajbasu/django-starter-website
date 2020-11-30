ea56cf2ca12045b1fd103fd253363fd5535be938
deeprajbasu <deepraj.basu@gmail.com>
2020-11-30 22:13:45 +0530

    third

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
     ]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 
